dd62ce2fe803d5236afb1191bc5862d3b35585eb
ia74 <aidenjs9@icloud.com>
2024-08-10 11:43:20 -0500

    Update for new

--- Clock.src/clock/client.js
+++ Clock.src/clock/client.js
@@ -3,17 +3,24 @@ const handle = () => {
 		if (button.getAttribute('data-interaction')) {
 			let dat = button.getAttribute('data-interaction');
 			dat = JSON.parse(dat);
+			if(button.id == 'editor-btn') return;
 			if (dat.type == 'clock.time') {
-				button.innerText = new Date(Date.now()).toLocaleTimeString();
+				let txt = button.querySelector('.button-text').querySelector('p');
+				txt.innerText = new Date(Date.now()).toLocaleTimeString();
 			} else if (dat.type == 'clock.date') {
-				button.innerText = new Date(Date.now()).toLocaleDateString();
+				let txt = button.querySelector('.button-text').querySelector('p');
+				txt.innerText = new Date(Date.now()).toLocaleDateString();
 			} else if (dat.type == 'clock.time.24') {
-				button.innerText = new Date(Date.now()).toLocaleTimeString('en-US', { hour12: false });
+				let txt = button.querySelector('.button-text').querySelector('p');
+				txt.innerText = new Date(Date.now()).toLocaleTimeString('en-US', { hour12: false });
 			}
 		}
 	});
 };
 handle();
+universal.listenFor('page_change', () => {
+	handle();
+})
 setInterval(() => {
 	handle();
 }, 1000)
